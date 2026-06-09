4954e1b96401f04f4d6a5d1e0c85763278b6f416
Rafael <rafadrummer1@gmail.com>
2026-06-09 16:13:23 -0300

    exercícios python

--- exrcicios.py
+++ exrcicios.py
@@ -2,10 +2,21 @@ print("--- ALUGUEL DE FILMES ---")
 nome = input("Digite seu nome: ")
 cpf = int(input("Digite seu cpf: "))
 idade = int(input("digite sua idade: "))
-plano_usuario = input("Digite seu plano bronze,prata ou ouro...")
-if plano_usuario.lower == "bronze" and idade >= 18:
-    print("teste")
-elif plano_usuario.lower == "bronze" and idade < 18:
-    print("teste")
-if plano_usuario.lower == "prata" and idade >= 18:
+plano_usuario = input("Digite seu plano bronze,prata ou ouro...").lower()
+
+if plano_usuario == "bronze" :
+    print("Forrest Gump",
+    "Toy Story",
+    "O Rei Leão",
+    "Jurassic Park",
+    "Titanic",
+    "Harry Potter e a Pedra Filosofal",
+    "De Volta para o Futuro")
+if plano_usuario == "prata"  :
     print("Matrix","Gladiador","Os Vingadores")
+
+if plano_usuario == "ouro" :
+    print("Interestelar","Clube da Luta","A Origem")
+
+elif plano_usuario == "ouro" or "prata" and  idade >= 18 :
+    print("O Exorcista","It — A Coisa")
